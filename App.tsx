16f848a66deabfbfc2944251a8144826027c0d7a
askozyra <khnu2019@gmail.com>
2023-02-06 21:08:53 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Init layout</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
