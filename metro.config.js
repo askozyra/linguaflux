289e83eb5694a5c26f8d85d7fa8b72510a904917
askozyra <khnu2019@gmail.com>
2023-02-10 23:12:24 +0300

    _2_: included kitten ui; provided svg icons import

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,11 @@
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
- *
- * @format
- */
+*
+* @format
+*/
+
+const { getDefaultConfig } = require('metro-config');
 
 module.exports = {
   transformer: {
@@ -15,3 +17,18 @@ module.exports = {
     }),
   },
 };
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
